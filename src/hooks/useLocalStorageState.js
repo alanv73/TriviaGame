84b4dd37013708711c4a8e718dbd4fc2e24a3cdc
alanv73@gmail.com <alanv73@gmail.com>
2021-03-26 13:12:34 -0400

    add local storage hook

---
+++ src/hooks/useLocalStorageState.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorageState = (key, defaultVal) => {
+    // make piece of state based off of value in local storage (or default value)
+    const [state, setState] = useState(() => {
+        let val;
+        try{
+            val = JSON.parse(
+                window.localStorage.getItem(key) || String(defaultVal)
+            );
+        } catch (e) {
+            val = defaultVal;
+        }
+        return val;
+    });
+
+    // useEffect to update local storage when state changes
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(state));
+        // eslint-disable-next-line
+    }, [state]);
+
+    return [state, setState];
+}
+
+export default useLocalStorageState;
