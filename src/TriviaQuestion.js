114daab30b67ddbf6808a1c106cca95fb1d6d870
alanv73@gmail.com <alanv73@gmail.com>
2021-03-26 13:14:04 -0400

    use local storage hook

--- src/TriviaQuestion.js
+++ src/TriviaQuestion.js
@@ -4,13 +4,14 @@ import defaultCategories from './categories';
 import { shuffle, getTriviaQuestion, getRandomCategory } from './helpers';
 import useIncrementState from './hooks/useIncrementState';
 import useToggleState from './hooks/useToggleState';
+import useLocalStorageState from './hooks/useLocalStorageState';
 import './TriviaGame.css';
 
 function TriviaQuestion({questionCount=1, difficulty='medium'}) {
     const [score, incrementScore] = useIncrementState(0);
-    const [categories, setCategories] = useState(
-        window.localStorage.getItem("categories") 
-        || defaultCategories.map(c => ({...c, selected: true}))
+    const [categories, setCategories] = useLocalStorageState(
+        "categories", 
+        defaultCategories.map(c => ({...c, selected: true}))
     );
     const [question, setQuestion] = useState();
     const [showBack, toggleShowBack] = useToggleState(false);
@@ -44,9 +45,14 @@ function TriviaQuestion({questionCount=1, difficulty='medium'}) {
 
     const handleBackClick = async () => {
         toggleShowBack();
-        if(isAnswerCorrect) 
-            toggleIsAnswerCorrect();
-        await getNewQuestion();
+        
+        console.log('getting question');
+        setTimeout(async () => {
+            await getNewQuestion();
+            if(isAnswerCorrect) 
+                toggleIsAnswerCorrect();
+        }, 200);
+        
 
     }
 
@@ -56,15 +62,13 @@ function TriviaQuestion({questionCount=1, difficulty='medium'}) {
                 <div className="triviagame-score">
                     <h2 className="score">Score: {score}</h2>
                 </div>
-                <div className="triviagame-content">
-                    <QuestionCard 
-                        {...question}
-                        handleClick={handleGuess}
-                        isCorrect={isAnswerCorrect}
-                        clickBack={handleBackClick}
-                        showBack={showBack}
-                    />
-                </div>
+                <QuestionCard 
+                    {...question}
+                    handleClick={handleGuess}
+                    isCorrect={isAnswerCorrect}
+                    clickBack={handleBackClick}
+                    showBack={showBack}
+                />
             </div>
         )
         return null;
